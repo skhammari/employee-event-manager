1702b983b118a2d3c1c28244075f8e7b96ef8d87
saeed <skhammari@gmail.com>
2025-01-26 00:25:14 +0330

    add install script

--- deploy.sh
+++ deploy.sh
@@ -15,15 +15,30 @@ sed -i "s/APP_DEBUG=.*/APP_DEBUG=false/" .env
 sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=$DB_PASSWORD/" .env
 sed -i "s/DB_ROOT_PASSWORD=.*/DB_ROOT_PASSWORD=$DB_ROOT_PASSWORD/" .env
 
-# Install PHP and required extensions
+# Add PHP repository and install PHP
 sudo apt-get update
-sudo apt-get install -y php8.2-cli php8.2-gd php8.2-intl php8.2-zip php8.2-mbstring php8.2-xml php8.2-curl
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install -y php8.2 \
+    php8.2-cli \
+    php8.2-gd \
+    php8.2-intl \
+    php8.2-zip \
+    php8.2-mbstring \
+    php8.2-xml \
+    php8.2-curl \
+    php8.2-mysql \
+    php8.2-common \
+    php8.2-bcmath \
+    php8.2-fpm \
+    unzip
 
-# Install Composer locally
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php --quiet
-rm composer-setup.php
-mv composer.phar /usr/local/bin/composer
+# Install Composer
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+# Install dependencies
+composer install --no-dev --optimize-autoloader
 
 # Install Laravel Sail
 composer require laravel/sail --dev
